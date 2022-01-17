67eab13827b5814e1b41ee8f43bfa7a346b05bc5
FibrinLab <fibrinlab@gmail.com>
2022-01-17 23:58:15 +0100

    Contract Balance

--- contracts/DistTokenGovernance.sol
+++ contracts/DistTokenGovernance.sol
@@ -32,6 +32,12 @@ contract DistTokenGovernance is Initializable, ERC20, ERC20Detailed, ReentrancyG
     address private cToken;
     // Dist rebalancer current implementation address
     address public rebalancer;
+    // Address collecting underlying fees
+    address public feeAddress;
+    // Max unlent assets percentage for gas friendly swaps
+    uint256 public maxUnlentPerc; // 100000 == 100% -> 1000 == 1%
+    // Current fee on interest gained
+    uint256 public fee;
     // eg. [COMPAddress, CRVAddress, ...]
     address[] public govTokens;
     // eg. [cTokenAddress, iTokenAddress, ...]
@@ -45,6 +51,22 @@ contract DistTokenGovernance is Initializable, ERC20, ERC20Detailed, ReentrancyG
     // Addresses for stkAAVE distribution from Aave
     address public constant stkAAVE = address(0x4da27a545c0c5B758a6BA100e3a049001de870f5);
     address private aToken;
+
+    // ########## DistToken updates
+    // Dist governance token
+    address public constant DIST = address(0x875773784Af8135eA0ef43b5a374AaD105c5D39e);
+    // Compound governance token
+    address public constant COMP = address(0xc00e94Cb662C3520282E6f5717214004A7f26888);
+    uint256 private constant FULL_ALLOC = 100000;
+
+    // Dist distribution controller
+    address public constant distController = address(0x275DA8e61ea8E02d51EDd8d0DC5c0E62b4CDB0BE);
+    // oracle used for calculating the avgAPR with gov tokens
+    address public oracle;
+    // eg cDAI -> COMP
+    mapping(address => address) private protocolTokenToGov;
+    // last allocations submitted by rebalancer
+    uint256[] private lastRebalancerAllocations;
 
     // ERROR MESSAGES:
     // 0 = is 0
@@ -129,14 +151,143 @@ contract DistTokenGovernance is Initializable, ERC20, ERC20Detailed, ReentrancyG
     }
 
     /**
-    * It allows owner to set the IdleRebalancerV3_1 address
+    * It allows owner to set the DistRebalancerV3_1 address
     *
-    * @param _rebalancer : new IdleRebalancerV3_1 address
+    * @param _rebalancer : new DistRebalancerV3_1 address
     */
     function setRebalancer(address _rebalancer)
         external onlyOwner {
         require((rebalancer = _rebalancer) != address(0), "0");
     }
 
+    /**
+    * It allows owner to set the fee (1000 == 10% of gained interest)
+    *
+    * @param _fee : fee amount where 100000 is 100%, max settable is 10%
+    */
+    function setFee(uint256 _fee)
+        external onlyOwner {
+        // 100000 == 100% -> 10000 == 10%
+        require((fee = _fee) <= FULL_ALLOC / 10, "5");
+    }
+
+    /**
+    * It allows owner to set the fee address
+    *
+    * @param _feeAddress : fee address
+    */
+    function setFeeAddress(address _feeAddress)
+        external onlyOwner {
+        require((feeAddress = _feeAddress) != address(0), "0");
+    }
+
+    /**
+    * It allows owner to set the oracle address for getting avgAPR
+    *
+    * @param _oracle : new oracle address
+    */
+    function setOracleAddress(address _oracle)
+        external onlyOwner {
+        require((oracle = _oracle) != address(0), "0");
+    }
+
+    /**
+    * It allows owner to set the max unlent asset percentage (1000 == 1% of unlent asset max)
+    *
+    * @param _perc : max unlent perc where 100000 is 100%
+    */
+    function setMaxUnlentPerc(uint256 _perc)
+        external onlyOwner {
+        require((maxUnlentPerc = _perc) <= 100000, "5");
+    }
+
+    /**
+    * Used by Rebalancer to set the new allocations
+    *
+    * @param _allocations : array with allocations in percentages (100% => 100000)
+    */
+    function setAllocations(uint256[] calldata _allocations) external {
+        require(msg.sender == rebalancer || msg.sender == owner(), "6");
+        _setAllocations(_allocations);
+    }
+
+    /**
+    * Used by Rebalancer or in openRebalance to set the new allocations
+    *
+    * @param _allocations : array with allocations in percentages (100% => 100000)
+    */
+    function _setAllocations(uint256[] memory _allocations) internal {
+        require(_allocations.length == allAvailableTokens.length, "2");
+        uint256 total;
+        for (uint256 i = 0; i < _allocations.length; i++) {
+            total = total.add(_allocations[i]);
+        }
+        lastRebalancerAllocations = _allocations;
+        require(total == FULL_ALLOC, "7");
+    }
+
+    // view
+    /**
+    * Get latest allocations submitted by rebalancer
+    *
+    * @return : array of allocations ordered as allAvailableTokens
+    */
+    function getAllocations() external view returns (uint256[] memory) {
+        return lastRebalancerAllocations;
+    }
+
+    /**
+    * Get currently used gov tokens
+    *
+    * @return : array of govTokens supported
+    */
+    function getGovTokens() external view returns (address[] memory) {
+        return govTokens;
+    }
+
+    /**
+    * Get currently used protocol tokens (cDAI, aDAI, ...)
+    *
+    * @return : array of protocol tokens supported
+    */
+    function getAllAvailableTokens() external view returns (address[] memory) {
+        return allAvailableTokens;
+    }
+
+    /**
+    * Get gov token associated to a protocol token eg protocolTokenToGov[cDAI] = COMP
+    *
+    * @return : address of the gov token
+    */
+    function getProtocolTokenToGov(address _protocolToken) external view returns (address) {
+        return protocolTokenToGov[_protocolToken];
+    }
+
+    /**
+    * DistToken price for a user considering fees, in underlying
+    * this is useful when you need to redeem exactly X underlying
+    *
+    * @return : price in underlying token counting fees for a specific user
+    */
+    function tokenPriceWithFee(address user)
+        external view
+        returns (uint256 priceWFee) {
+            uint256 userAvgPrice = userAvgPrices[user];
+            priceWFee = _tokenPrice();
+            if (userAvgPrice != 0 && priceWFee > userAvgPrice) {
+                priceWFee = priceWFee.mul(FULL_ALLOC).sub(fee.mul(priceWFee.sub(userAvgPrice))).div(FULL_ALLOC);
+            }
+    }
+
+    /**
+    * DistToken price calculation, in underlying
+    *
+    * @return : price in underlying token
+    */
+    function tokenPrice()
+        external view
+        returns (uint256) {
+        return _tokenPrice();
+    }
 
 }
