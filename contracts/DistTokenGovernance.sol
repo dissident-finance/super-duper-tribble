e3e95955d60dc260acc923d406f84e4fd863c3c2
FibrinLab <fibrinlab@gmail.com>
2022-01-17 22:31:22 +0100

    token helper working

--- contracts/DistTokenGovernance.sol
+++ contracts/DistTokenGovernance.sol
@@ -23,6 +23,15 @@ contract DistTokenGovernance is Initializable, ERC20, ERC20Detailed, ReentrancyG
   using SafeERC20 for IERC20;
   using SafeMath for uint256;
 
+  // State variables
+  // eg. DAI address
+  address public token;
+  // eg. iDAI address
+  address private iToken;
+  // eg. cDAI address
+  address private cToken;
+  // Idle rebalancer current implementation address
+  address public rebalancer;
   // eg. [COMPAddress, CRVAddress, ...]
   address[] public govTokens;
   // eg. [cTokenAddress, iTokenAddress, ...]
@@ -30,6 +39,12 @@ contract DistTokenGovernance is Initializable, ERC20, ERC20Detailed, ReentrancyG
   // eg. cTokenAddress => DistCompoundAddress
   mapping(address => address) public protocolWrappers;
 
+  // DistToken helper address
+  address public tokenHelper;
+
+  // Addresses for stkAAVE distribution from Aave
+  address public constant stkAAVE = address(0x4da27a545c0c5B758a6BA100e3a049001de870f5);
+  address private aToken;
 
   // ERROR MESSAGES:
   // 0 = is 0
@@ -83,7 +98,45 @@ contract DistTokenGovernance is Initializable, ERC20, ERC20Detailed, ReentrancyG
        allAvailableTokens = protocolTokens;
    }
 
+    /**
+   * It allows owner to set the cToken address
+   *
+   * @param _cToken : new cToken address
+   */
+    function setCToken(address _cToken)
+        external onlyOwner {
+        require((cToken = _cToken) != address(0), "0");
+    }
 
+    /**
+    * It allows owner to set the aToken address
+    *
+    * @param _aToken : new aToken address
+    */
+    function setAToken(address _aToken)
+        external onlyOwner {
+        require((aToken = _aToken) != address(0), "0");
+    }
+
+    /**
+   * It allows owner to set the tokenHelper address
+   *
+   * @param _tokenHelper : new tokenHelper address
+   */
+    function setTokenHelper(address _tokenHelper)
+        external onlyOwner {
+        require((tokenHelper = _tokenHelper) != address(0), "0");
+    }
+
+    /**
+   * It allows owner to set the IdleRebalancerV3_1 address
+   *
+   * @param _rebalancer : new IdleRebalancerV3_1 address
+   */
+    function setRebalancer(address _rebalancer)
+        external onlyOwner {
+        require((rebalancer = _rebalancer) != address(0), "0");
+    }
 
 
 }
